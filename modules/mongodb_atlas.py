2b468412a22b135d0f2216c2224bc694e0e9c6a9
nightpanda2810 <nightpanda2810@gmail.com>
2024-05-14 16:32:38 -0400

    Added motor for async mongo data fetching.

--- modules/mongodb_atlas.py
+++ modules/mongodb_atlas.py
@@ -1,11 +1,11 @@
 """Module for uploading die data to MongoDB Atlas"""
 
 # Standard library imports
+import asyncio
 
 # Public library imports
 from pymongo.mongo_client import MongoClient
-
-# import asyncio
+import motor.motor_asyncio
 
 # Personal library imports
 # Module imports
@@ -32,21 +32,27 @@ def upload_die_result(state, uri, data):
         if state.config["DEBUG"]:
             print(upload_data)
             print(f"Uploaded data with id: {result.inserted_id}")
-        pass
     except Exception:
         raise
 
 
-def show_die_data(uri):
-    client = MongoClient(uri)
+async def show_die_data_stuff(uri):
+    client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+    db = client["pixels-die-stats"]
+    collection = db["pixels-die-stats"]
+    try:
+        cursor = collection.find()
+        data = []
+        async for document in cursor:
+            data.append(document)
+        return [{k: v for k, v in doc.items() if k != "_id"} for doc in data]
+    except Exception:
+        raise
+
+
+async def show_die_data(state):
+    uri = state.config["mongodb_atlas_uri"]
+    # print(state.config["mongodb_atlas_uri"])
     while True:
-        try:
-            db = client["pixels-die-stats"]
-            collection = db["pixels-die-stats"]
-            cursor = collection.find()
-            data = []
-            for document in cursor:
-                data.append(document)
-            return [{k: v for k, v in doc.items() if k != "_id"} for doc in data]
-        except Exception:
-            raise
+        await asyncio.sleep(5)
+        state.mongo_data = await show_die_data_stuff(uri)
