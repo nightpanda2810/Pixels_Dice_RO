53e2c64e7dfd10409888aea6bd23b8dd9c2a98ac
nightpanda2810 <nightpanda2810@gmail.com>
2024-05-14 11:55:12 -0400

    New method of connecting to dice. Self made.

--- modules/connect_dice.py
+++ modules/connect_dice.py
@@ -1,90 +1,84 @@
 # Standard library imports
 import asyncio
-import time
+import binascii
 
 # Public library imports
-from nat20 import scan_for_dice
+from bleak import BleakScanner, BleakClient
 
 # Personal library imports
-# Module imports
-from modules.mongodb_atlas import upload_die_result
+from pandalibs.yaml_importer import get_configuration_data
+from pandalibs.pprint_nosort import pp
 
-# Initialized variables
+DICE_NAME = "Aurora"  # Replace with your dice device name
 
-
-# Remainder of the code
-async def find_and_connect_to_dice(state):
-    """Find and connect to all dice objects, and connect to those specified in configuration file."""
-    dice_state = {}
-
-    async def connect_die(available_die, die_id):
-        """Connect to a die object and add it to the shared state."""
-        try:
-            if available_die.pixel_id == int(die_id, 16):
-                state.die_data[die_id] = {
-                    "connected_pixel": available_die.hydrate(),
-                    "die_name": available_die.name,
-                    "battery": available_die.batt_level,
-                    "session_date": state.session_date,
-                    "time_rolled": "",
-                    "die_type": f"D{available_die.face_count}",
-                    "last_roll": 0,
-                }
-                await state.die_data[die_id]["connected_pixel"].connect()
-                state.die_connected = True
-                if state.config["DEBUG"]:
-                    print(f"Connected to: {state.die_data[die_id]['connected_pixel'].name} ID: {state.die_data[die_id]['connected_pixel'].pixel_id:x}")
-            if state.die_data[die_id]["connected_pixel"].name not in dice_state:
-                dice_state[state.die_data[die_id]["connected_pixel"].name] = {"prev_state": 0, "current_state": state.die_data[die_id]["connected_pixel"].roll_state}
-            else:
-                if state.config["DEBUG"]:
-                    current_time = time.strftineme("%H:%M:%S")
-                    print(f"Didn't connect. {current_time}")
-        except Exception:
-            if state.config["DEBUG"]:
-                print("Exception in connect_die.")
-                raise
-            raise
-
-    async for available_dice in scan_for_dice():
-        if state.config["DEBUG"]:
-            print(f"Found die - Name: {available_dice.name} ID: {available_dice.pixel_id:x}")
-        # This will need refactored for multiple dice. Needs the while loop up here.
-        for die_name, die_value in state.config["dice"].items():
-            if state.config["DEBUG"]:
-                print(f"Attempting to connect to {die_name} ID: {die_value}...")
-            if die_value:
-                await connect_die(available_dice, die_value)
-            while True:
-                try:
-                    await asyncio.sleep(0.1)
-                    if state.config["DEBUG_LOOP"]:
-                        print("Entering loop.")
-                    current_state = state.die_data[die_value]["connected_pixel"].roll_state
-                    prev_state = dice_state[state.die_data[die_value]["connected_pixel"].name]["prev_state"]
-                    if state.config["DEBUG_LOOP"]:
-                        await asyncio.sleep(1)
-                        print(f"Current state: {current_state}")
-                        print(f"Previous state: {prev_state}")
-                    if prev_state == 3 and current_state == 1:
-                        # Checks if the die was recently rolled (3) and has finished rolling (1) before adjusting the roll face.
-                        state.die_data[die_value]["last_roll"] = state.die_data[die_value]["connected_pixel"].roll_face + 1
-                        state.die_data[die_value]["time_rolled"] = time.strftime("%H:%M:%S", time.localtime())
-                        # Upload to database.
-                        upload_die_result(state, state.config["mongodb_atlas_uri"], state.die_data[die_value])
-                        if state.config["DEBUG"]:
-                            print(f"{state.die_data[die_value]['connected_pixel'].name} rolled: {state.die_data[die_value]['last_roll']}")
-                            print(f"Battery: {state.die_data[die_value]['connected_pixel'].batt_level}")
-                            print(f"session_date: {state.session_date}")
-                    dice_state[state.die_data[die_value]["connected_pixel"].name]["prev_state"] = current_state
-                except Exception:
-                    if state.config["DEBUG"]:
-                        print("Exception in die loop.")
-                        raise
+CONFIG = get_configuration_data()
+DIE_CONFIG = get_configuration_data(file_name="die_raw_data.yaml")
+DIE_RESULTS = DIE_CONFIG["DIE_RESULTS"]
 
 
-async def show_die_data(state):
-    while True:
-        await asyncio.sleep(1)
-        if state.die_connected:
-            print(f"{state.die_data}")
+class Pixel_Die:
+    """Class for a single die."""
+
+    def __init__(self, name, shared_state):
+        self.die_name = name
+        self.shared_state = shared_state
+        self.die_device = None
+        self.client = None
+
+    def notification_handler(self, sender, data):
+        new_data = binascii.hexlify(data)
+        decoded_data = new_data.decode("ascii")
+        if decoded_data in DIE_RESULTS and decoded_data != "16000a":
+            result = DIE_RESULTS[decoded_data]
+            self.shared_state.die_data["39de7022"]["last_roll"] = result
+
+    async def scan_for_device(self):
+        die_connected = False
+        while not die_connected:
+            print("Scanning for devices...")
+            devices = await BleakScanner.discover()
+            for device in devices:
+                if device.name == self.die_name:
+                    self.die_device = device
+                    die_connected = True
+                    break
+            if not self.die_device:
+                print("Dice not found.")
+
+    async def connect_and_subscribe(self):
+        if not self.die_device:
+            print("No device to connect to")
+            return
+        async with BleakClient(self.die_device.address) as self.client:
+            print(f"Connected to {self.die_device.name}")
+            self.shared_state.die_data["39de7022"] = {
+                "connected_pixel": True,
+                "die_name": True,
+                "battery": True,
+                "session_date": True,
+                "time_rolled": True,
+                "die_type": True,
+                "last_roll": 0,
+            }
+            # Subscribe to notifications
+            for service in self.client.services:
+                for characteristic in service.characteristics:
+                    if "notify" in characteristic.properties:
+                        await self.client.start_notify(characteristic.uuid, self.notification_handler)
+            # Keep the script running to receive notifications
+            print("Waiting for notifications...")
+            await asyncio.sleep(43200)  # Adjust the sleep time as needed
+
+    async def run(self):
+        await self.scan_for_device()
+        if self.die_device:
+            await self.connect_and_subscribe()
+
+
+async def connect_to_dice(state):
+    for die_id, die_name in state.config["dice"].items():
+        print("Available Dice:")
+        if die_name:
+            print(f"Name: {die_id} value: {die_name}")
+            die = Pixel_Die(die_name, state)
+            await die.run()
