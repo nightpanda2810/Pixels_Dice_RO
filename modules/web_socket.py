ca7b873d53fd0a83e2fcd1f8816db9f001448e98
nightpanda2810 <nightpanda2810@gmail.com>
2024-05-14 18:30:35 -0400

    Added die roll average functionality.

--- modules/web_socket.py
+++ modules/web_socket.py
@@ -1,6 +1,7 @@
 # Standard library imports
 import asyncio
 import websockets
+import json
 
 # import random
 
@@ -19,12 +20,17 @@ async def handle_client(websocket, path, state):
             for die_id, die_name in state.config["dice"].items():
                 if die_name:
                     roll = state.die_data[die_id]["last_roll"]
-                    await websocket.send(str(roll))  # Send the roll value as a string
+                    average = state.average_rolls
+                    data = {
+                        "roll": roll,
+                        "average": average,
+                    }
+                    await websocket.send(json.dumps(data))  # Send the roll value as a string
                     await asyncio.sleep(1)  # Update every second (adjust as needed)
     except websockets.exceptions.ConnectionClosedOK:
         pass
-    except KeyError as e:
-        print(e)
+    except KeyError:
+        pass
     except Exception:
         raise
 
