fedb9bd91054b364fa6937b7f691fcbba7b9b7bf
nightpanda2810 <nightpanda2810@gmail.com>
2024-05-14 16:05:49 -0400

    Removed unnecessary function.

--- modules/mongodb_atlas.py
+++ modules/mongodb_atlas.py
@@ -1,3 +1,5 @@
+"""Module for uploading die data to MongoDB Atlas"""
+
 # Standard library imports
 
 # Public library imports
@@ -9,19 +11,6 @@ from pymongo.mongo_client import MongoClient
 # Module imports
 # Initialized variables
 # Remainder of the code
-
-
-async def test_mongo(uri):
-    # Create a new client and connect to the server
-    client = MongoClient(uri)
-
-    # Send a ping to confirm a successful connection
-    while True:
-        try:
-            client.admin.command("ping")
-            print("Pinged your deployment. You successfully connected to MongoDB!")
-        except Exception as e:
-            print(e)
 
 
 def extract_desired_data(data):
