64c4af0bec8dd0afcec9571189345ea31b4fc384
nightpanda2810 <nightpanda2810@gmail.com>
2024-05-14 15:08:24 -0400

    Changed from importing a function to inporting the class, then generating multiple die objects based on config. MULTIPLE DICE UNTESTED.

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ from pandalibs.yaml_importer import get_configuration_data
 from pandalibs.pprint_nosort import pp
 
 # Module imports
-from modules.connect_dice import connect_to_dice
+from modules.connect_dice import Pixel_Die
 from modules.shared_state import SharedState
 from modules.web_socket import start_server
 
 # Initialized variables
 shared_state = SharedState()
 shared_state.config = get_configuration_data()
+shared_state.die_config = get_configuration_data(file_name="die_raw_data.yaml")
 
 # Remainder of the code
 # Print current configuration if DEBUG is true.
@@ -22,12 +23,23 @@ if shared_state.config["DEBUG"]:
     print("End configuration information.\n")
 
 
+async def connect_to_dice():
+    all_dice = []
+    print("Configured Dice:")
+    for die_id, die_name in shared_state.config["dice"].items():
+        if die_name:
+            print(f"Name: {die_id} value: {die_name}")
+            all_dice.append(Pixel_Die(die_id, die_name, shared_state))
+    for die in all_dice:
+        await die.run()
+
+
 # Main event loop function.
 async def main():
     """Main event loop."""
     await asyncio.gather(
         start_server(shared_state),
-        connect_to_dice(shared_state),
+        connect_to_dice(),
     )
 
 
