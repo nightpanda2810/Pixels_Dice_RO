cdd2de27311cac7ff95804c55f5fb6b87456e382
nightpanda2810 <nightpanda2810@gmail.com>
2024-05-25 17:55:46 -0400

    Updated build commands.

---
+++ extras_extra/build.py
@@ -0,0 +1,5 @@
+# Without terminal window.
+# nuitka --standalone --onefile --windows-disable-console --windows-icon-from-ico=./extras/logo.ico --plugin-enable=pylint-warnings --include-module=bleak --include-module=websockets.legacy --include-module=websockets.legacy.server --include-data-dir=./extras=./extras .\main.py
+
+# With terminal window.
+# nuitka --standalone --onefile --windows-icon-from-ico=./extras/logo.ico --plugin-enable=pylint-warnings --include-module=bleak --include-module=websockets.legacy --include-module=websockets.legacy.server --include-data-dir=./extras=./extras .\main.py
