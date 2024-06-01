1e90ca46cc019644b4f90657253f4ad7569fd642
nightpanda2810 <nightpanda2810@gmail.com>
2024-06-01 13:10:35 -0400

    Build file.

--- extras_extra/build.py
+++ extras_extra/build.py
@@ -3,3 +3,49 @@
 
 # With terminal window.
 # nuitka --standalone --onefile --windows-icon-from-ico=./extras/logo.ico --plugin-enable=pylint-warnings --include-module=bleak --include-module=websockets.legacy --include-module=websockets.legacy.server --include-data-dir=./extras=./extras .\main.py
+
+import subprocess
+import sys
+
+
+def compile_with_nuitka():
+    command = (
+        "nuitka "
+        "--standalone "
+        "--onefile "
+        "--windows-disable-console "
+        "--windows-icon-from-ico=./extras/logo.ico "
+        "--plugin-enable=pylint-warnings "
+        "--include-module=bleak "
+        "--include-module=websockets.legacy "
+        "--include-module=websockets.legacy.server "
+        "--include-data-dir=./extras=./extras "
+        "./main.py"
+    )
+
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+
+    try:
+        while True:
+            output = process.stdout.readline()
+            if output == "" and process.poll() is not None:
+                break
+            if output:
+                print(output.strip())
+                sys.stdout.flush()
+
+        stderr_output = process.communicate()[1]
+        if stderr_output:
+            print(stderr_output, end="")
+            sys.stdout.flush()
+
+        if process.returncode != 0:
+            raise subprocess.CalledProcessError(process.returncode, command)
+
+    except subprocess.CalledProcessError:
+        print("Compilation failed:")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    compile_with_nuitka()
