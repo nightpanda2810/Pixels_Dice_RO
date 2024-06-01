6728802ffc6ff81cf0f590aedde7e954d3acdca9
nightpanda2810 <nightpanda2810@gmail.com>
2024-06-01 10:13:17 -0400

    Added initial sound effects module.

---
+++ modules/sound_effects.py
@@ -0,0 +1,33 @@
+import vlc
+import random
+from pandalibs.yaml_importer import get_configuration_data
+
+# import variables.sounds as sounds
+import os
+import sys
+
+
+# Import sounds
+def get_application_path():
+    if getattr(sys, "frozen", False):
+        return os.path.dirname(sys.executable)
+    else:
+        return os.path.dirname(os.path.abspath(sys.argv[0]))
+
+
+def play_sound(die_roll):
+    try:
+        sound_import = get_configuration_data()
+        bad_sounds = sound_import["bad_sounds"]
+        good_sounds = sound_import["good_sounds"]
+        any_sounds = sound_import["any_sounds"]
+        if die_roll == 1:
+            url = random.choice(bad_sounds)
+        elif die_roll == 20:
+            url = random.choice(good_sounds)
+        else:
+            url = random.choice(any_sounds)
+            pass
+        vlc.MediaPlayer(url).play()
+    except Exception as e:
+        print(f"There was an error in sound_effects.play_sound: {e}")
